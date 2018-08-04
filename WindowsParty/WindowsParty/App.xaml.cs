f7aba4b6ac8ac44c8943e45f2d2e4546c7de935f
Epsil0neR <epsil0neris@gmail.com>
2018-08-04 21:49:54 +0300

    Adds IoC configuration

--- WindowsParty/WindowsParty/App.xaml.cs
+++ WindowsParty/WindowsParty/App.xaml.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using Unity;
 
 namespace WindowsParty
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        /// <inheritdoc />
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Configuration.ConfigureIoC();
+        }
     }
 }
---
+++ WindowsParty/WindowsParty/Configuration.cs
@@ -0,0 +1,21 @@
+﻿using WindowsParty.ApiServices;
+using Unity;
+using Unity.Lifetime;
+
+namespace WindowsParty
+{
+    internal static class Configuration
+    {
+        private static IUnityContainer _container = _container ?? (_container = new UnityContainer());
+
+        public static IUnityContainer Container => _container;
+
+        public static void ConfigureIoC()
+        {
+            var c = Container;
+
+            //Singleton
+            c.RegisterType<IPlaygroundService, PlaygroundService>(new ContainerControlledLifetimeManager());
+        }
+    }
+}
