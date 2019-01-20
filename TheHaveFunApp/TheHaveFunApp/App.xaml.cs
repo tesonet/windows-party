4d4502a64e185b25e8f652a780112c2a4cae3b34
Julius Degutis <julius.degutis@gmail.com>
2019-01-20 22:17:13 +0200

    created solution (remembering old old times)

---
+++ TheHaveFunApp/TheHaveFunApp/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml;
+using CommonServiceLocator;
+using Prism;
+using Prism.Ioc;
+using Prism.Regions;
+using Prism.Unity;
+using TheHaveFunApp.ViewModels;
+using TheHaveFunApp.Views;
+
+namespace TheHaveFunApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+
+
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            IRegionManager regionManager = ServiceLocator.Current.GetInstance<IRegionManager>();
+            regionManager.AddToRegion("MainRegion", this.Container.Resolve<LoginPage>());
+        }
+
+
+        protected override Window CreateShell()
+        {
+            return new MainWindow();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<LoginPage, LoginPageViewModel>();
+        }
+    }
+}
---
+++ TheHaveFunApp/TheHaveFunApp/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHaveFunApp.ViewModels
+{
+  public  class LoginPageViewModel
+    {
+        public LoginPageViewModel()
+        {
+            Debug.WriteLine("Created");
+        }
+    }
+}
